7946e11646a4d5ee22077f596f6c43b412416add
Restyled.io <commits@restyled.io>
2020-01-26 20:47:41 +0000

    Restyled by clang-format

--- source.h
+++ source.h
@@ -7,16 +7,16 @@
 #include <vector>
 
 struct FSFile {
-    std::string path;
-    std::string code;
-    FSFile(const std::string path, const std::string code)
-        : path(path), code(code) {};
-    FSFile &operator=(const FSFile &other);
+  std::string path;
+  std::string code;
+  FSFile(const std::string path, const std::string code)
+      : path(path), code(code){};
+  FSFile &operator=(const FSFile &other);
 };
 class SourceManager {
 public:
-    std::vector<FSFile> sources;
-    SourceManager() = default;
-    void open(const std::string &path);
-    // void open_(std::experimental::filesystem::path path);
+  std::vector<FSFile> sources;
+  SourceManager() = default;
+  void open(const std::string &path);
+  // void open_(std::experimental::filesystem::path path);
 };
