48d93855eea546c4a3ac2a71ac68abacc86397e7
Restyled.io <commits@restyled.io>
2020-01-26 20:46:45 +0000

    Restyled by clang-format

--- source.cpp
+++ source.cpp
@@ -1,16 +1,17 @@
 #include "source.h"
 #include "llvm/Support/raw_ostream.h"
 
-
-
-
-void SourceManager::open(const std::string& path) {
-    std::ifstream in(path);
-    if(!in.is_open())serror(Error_e::FileNExists,std::string("File ") + path + " doesn't exist.");
-    if(path.find(".fs")==path.npos) serror(Error_e::MustbeFsFile,"File must have .fs extension!");
-    std::string code((std::istreambuf_iterator<char>(in)),std::istreambuf_iterator<char>());
-    in.close();
-    sources.push_back(FSFile(path,code+"\0\0"));
+void SourceManager::open(const std::string &path) {
+  std::ifstream in(path);
+  if (!in.is_open())
+    serror(Error_e::FileNExists,
+           std::string("File ") + path + " doesn't exist.");
+  if (path.find(".fs") == path.npos)
+    serror(Error_e::MustbeFsFile, "File must have .fs extension!");
+  std::string code((std::istreambuf_iterator<char>(in)),
+                   std::istreambuf_iterator<char>());
+  in.close();
+  sources.push_back(FSFile(path, code + "\0\0"));
 }
 /*
 void SourceManager::open_(std::experimental::filesystem::path path) {
@@ -22,13 +23,14 @@ void SourceManager::open_(std::experimental::filesystem::path path) {
         error(ERR_ONLY_FS_FILES,"Only files with .fs extension are allowed!");
     }
     std::ifstream in(path);
-    std::string code((std::istreambuf_iterator<char>(in)),std::istreambuf_iterator<char>());
+    std::string
+code((std::istreambuf_iterator<char>(in)),std::istreambuf_iterator<char>());
     in.close();
     sources.push_back(FSFile(path,code));
 }
 */
-FSFile& FSFile::operator=(const FSFile& other) {
-    code=other.code;
-    path=other.path;
-    return *this;
+FSFile &FSFile::operator=(const FSFile &other) {
+  code = other.code;
+  path = other.path;
+  return *this;
 }
