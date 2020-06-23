074057793f7f9831336e7afc3090f64abc11fde6
Restyled.io <commits@restyled.io>
2020-06-23 14:29:33 +0000

    Restyled by astyle

--- src/error.cpp
+++ src/error.cpp
@@ -5,81 +5,81 @@
 #include <string>
 
 void serror(Error_e code, const std::string &msg) {
-  llvm::formatted_raw_ostream ro = llvm::formatted_raw_ostream(llvm::outs());
-  ro.changeColor(llvm::raw_ostream::RED, true, false);
-  ro.write("error: ", 7);
-  ro.resetColor();
-  ro.write(msg.c_str(), msg.size());
-  ro.flush();
-  std::exit(static_cast<int>(code));
+    llvm::formatted_raw_ostream ro = llvm::formatted_raw_ostream(llvm::outs());
+    ro.changeColor(llvm::raw_ostream::RED, true, false);
+    ro.write("error: ", 7);
+    ro.resetColor();
+    ro.write(msg.c_str(), msg.size());
+    ro.flush();
+    std::exit(static_cast<int>(code));
 }
 
 RespawnedCode respawn(const FSFile &file, unsigned int pos) {
-  auto file_name = file.path;
-  auto it = file.code.begin();
-  auto end = file.code.end();
-  int line = 1;
-  int col = 1;
-  while (pos--) {
-    if (*it++ == '\n') {
-      line++;
-      col = 1;
-    } else {
-      col++;
+    auto file_name = file.path;
+    auto it = file.code.begin();
+    auto end = file.code.end();
+    int line = 1;
+    int col = 1;
+    while (pos--) {
+        if (*it++ == '\n') {
+            line++;
+            col = 1;
+        } else {
+            col++;
+        }
     }
-  }
-  auto start = it;
-  while (it != end && *it++ != '\n') { /*empty body*/
-  }
-  auto code = std::string(start, it);
-  return RespawnedCode(line, col, code, file_name);
+    auto start = it;
+    while (it != end && *it++ != '\n') { /*empty body*/
+    }
+    auto code = std::string(start, it);
+    return RespawnedCode(line, col, code, file_name);
 }
 
 static void err_impl(Error_e code, const FSFile &file, const SourceLocation &sl,
                      const std::string &msg, llvm::raw_ostream::Colors color,
                      const std::string &title) {
-  auto rsc = respawn(file, sl.pos);
-  std::string s = ": " + msg + "\n" + rsc.code + "\n";
-  llvm::formatted_raw_ostream ro(llvm::outs());
-  ro.changeColor(color, true, false);
-  ro.write(title.c_str(), title.size());
-  ro.resetColor();
-  ro.write(s.c_str(), s.size());
-  for (int i = 0; i < rsc.col; i++) {
-    llvm::outs() << " ";
-  }
-  llvm::outs() << "^\n"
-               << "\n";
+    auto rsc = respawn(file, sl.pos);
+    std::string s = ": " + msg + "\n" + rsc.code + "\n";
+    llvm::formatted_raw_ostream ro(llvm::outs());
+    ro.changeColor(color, true, false);
+    ro.write(title.c_str(), title.size());
+    ro.resetColor();
+    ro.write(s.c_str(), s.size());
+    for (int i = 0; i < rsc.col; i++) {
+        llvm::outs() << " ";
+    }
+    llvm::outs() << "^\n"
+                 << "\n";
 }
 void error(Error_e code, const FSFile &file, const SourceLocation &sl,
            const std::string &msg) {
 
-  err_impl(code, file, sl, msg, llvm::raw_ostream::RED, "error: ");
+    err_impl(code, file, sl, msg, llvm::raw_ostream::RED, "error: ");
 }
 void warning(Error_e code, const FSFile &file, const SourceLocation &sl,
              const std::string &msg) {
-  err_impl(code, file, sl, msg, llvm::raw_ostream::MAGENTA, "warning:");
+    err_impl(code, file, sl, msg, llvm::raw_ostream::MAGENTA, "warning:");
 }
 void note(Error_e code, const FSFile &file, const SourceLocation &sl,
           const std::string &msg) {
-  err_impl(code, file, sl, msg, llvm::raw_ostream::BLACK, "note: ");
+    err_impl(code, file, sl, msg, llvm::raw_ostream::BLACK, "note: ");
 }
 
 void Error::UnkEscapeChar(const FSFile &file, const SourceLocation &sl,
                           const char ch) {
-  std::ostringstream os;
-  os << "Unknown escape character \'\\" << ch << "\'.";
+    std::ostringstream os;
+    os << "Unknown escape character \'\\" << ch << "\'.";
 
-  error(Error_e::UnkEsc, file, sl, os.str());
+    error(Error_e::UnkEsc, file, sl, os.str());
 }
 
 void Error::ExpectedToken(const FSFile &file, const SourceLocation &sl,
                           const std::string &msg) {
-  error(Error_e::ExpectedToken, file, sl, msg);
+    error(Error_e::ExpectedToken, file, sl, msg);
 }
 void Error::EmptyFnBody(const FSFile &file, const SourceLocation &sl) {
-  error(Error_e::EmptyFnBody, file, sl, "Empty Function Body");
+    error(Error_e::EmptyFnBody, file, sl, "Empty Function Body");
 }
 void Error::ImplementMe(std::string_view msg) {
-  llvm::outs() << "Error you didn't implement: " << msg.data() << "\n";
+    llvm::outs() << "Error you didn't implement: " << msg.data() << "\n";
 }
--- test/test_parser.cpp
+++ test/test_parser.cpp
@@ -5,135 +5,135 @@
 #include "gtest/gtest.h"
 
 TEST(parser, only_primary) {
-  FusionCtx ctx;
-  std::string code = "2";
-  auto file = FSFile("test", code);
-  auto lexer = Lexer(file, ctx);
-  lexer.lex();
-  auto tokens = lexer.tokens;
-  Token::Type tks[]{Token::Lit};
-  EXPECT_EQ(tokens.size(), 1);
-  for (int i = 0; i < tokens.size(); i++) {
-    EXPECT_EQ(tokens[i].type, tks[i]) << "i: " << i;
-  }
-  auto p = Parser(tokens, ctx, file);
-  auto expr = p.parse_expr();
-  ASSERT_NE(expr, nullptr) << "expr is null";
-  ASSERT_EQ(expr->type, AstType::ValExpr);
+    FusionCtx ctx;
+    std::string code = "2";
+    auto file = FSFile("test", code);
+    auto lexer = Lexer(file, ctx);
+    lexer.lex();
+    auto tokens = lexer.tokens;
+    Token::Type tks[] {Token::Lit};
+    EXPECT_EQ(tokens.size(), 1);
+    for (int i = 0; i < tokens.size(); i++) {
+        EXPECT_EQ(tokens[i].type, tks[i]) << "i: " << i;
+    }
+    auto p = Parser(tokens, ctx, file);
+    auto expr = p.parse_expr();
+    ASSERT_NE(expr, nullptr) << "expr is null";
+    ASSERT_EQ(expr->type, AstType::ValExpr);
 }
 
 TEST(parser, primary_and_op) {
-  FusionCtx ctx;
-  std::string code = "2+";
-  auto file = FSFile("test", code);
-  auto lexer = Lexer(file, ctx);
-  lexer.lex();
-  auto tokens = lexer.tokens;
-  Token::Type tks[]{Token::Lit, Token::Add};
-  EXPECT_EQ(tokens.size(), 2);
-  for (int i = 0; i < tokens.size(); i++) {
-    EXPECT_EQ(tokens[i].type, tks[i]) << "i: " << i;
-  }
-  auto p = Parser(tokens, ctx, file);
-  try {
-    auto expr = p.parse_expr();
-    ASSERT_EQ(expr, nullptr) << "expr is null";
-  } catch (const std::exception &e) {
-    ASSERT_EQ(1, 0) << "crashed";
-  }
+    FusionCtx ctx;
+    std::string code = "2+";
+    auto file = FSFile("test", code);
+    auto lexer = Lexer(file, ctx);
+    lexer.lex();
+    auto tokens = lexer.tokens;
+    Token::Type tks[] {Token::Lit, Token::Add};
+    EXPECT_EQ(tokens.size(), 2);
+    for (int i = 0; i < tokens.size(); i++) {
+        EXPECT_EQ(tokens[i].type, tks[i]) << "i: " << i;
+    }
+    auto p = Parser(tokens, ctx, file);
+    try {
+        auto expr = p.parse_expr();
+        ASSERT_EQ(expr, nullptr) << "expr is null";
+    } catch (const std::exception &e) {
+        ASSERT_EQ(1, 0) << "crashed";
+    }
 }
 TEST(parser, outofbounnds) {
-  FusionCtx ctx;
-  std::string code = "2 + 3";
-  auto file = FSFile("test", code);
-  auto lexer = Lexer(file, ctx);
-  lexer.lex();
-  auto tokens = lexer.tokens;
-  Token::Type tks[]{Token::Lit, Token::Add, Token::Lit};
-  EXPECT_EQ(tokens.size(), 3);
-  for (int i = 0; i < tokens.size(); i++) {
-    EXPECT_EQ(tokens[i].type, tks[i]) << "i: " << i;
-  }
-  /*
-          +
-        /   \
-       2     3
-  */
+    FusionCtx ctx;
+    std::string code = "2 + 3";
+    auto file = FSFile("test", code);
+    auto lexer = Lexer(file, ctx);
+    lexer.lex();
+    auto tokens = lexer.tokens;
+    Token::Type tks[] {Token::Lit, Token::Add, Token::Lit};
+    EXPECT_EQ(tokens.size(), 3);
+    for (int i = 0; i < tokens.size(); i++) {
+        EXPECT_EQ(tokens[i].type, tks[i]) << "i: " << i;
+    }
+    /*
+            +
+          /   \
+         2     3
+    */
 
-  auto p = Parser(tokens, ctx, file);
-  try {
+    auto p = Parser(tokens, ctx, file);
+    try {
+        auto expr = p.parse_expr();
+        ASSERT_NE(expr, nullptr) << "expr is null";
+        ASSERT_EQ(expr->type, AstType::BinExpr);
+        auto bin = reinterpret_cast<BinExpr *>(expr.get());
+        EXPECT_EQ(bin->lhs->type, AstType::ValExpr);
+        EXPECT_EQ(bin->rhs->type, AstType::ValExpr);
+    } catch (const std::exception &e) {
+        ASSERT_EQ(1, 0) << "crashed";
+    }
+}
+TEST(parser, addition) {
+    FusionCtx ctx;
+    std::string code = "2 + 3 * 2\n";
+    auto file = FSFile("test", code);
+    auto lexer = Lexer(file, ctx);
+    lexer.lex();
+    auto tokens = lexer.tokens;
+    EXPECT_EQ(tokens.size(), 5);
+    Token::Type tks[] {Token::Lit, Token::Add, Token::Lit, Token::Mul, Token::Lit};
+    for (int i = 0; i < tokens.size(); i++) {
+        EXPECT_EQ(tokens[i].type, tks[i]) << "i: " << i;
+    }
+    /*
+            *
+          /   \
+         +     2
+        / \
+       2   3
+    */
+    auto p = Parser(tokens, ctx, file);
     auto expr = p.parse_expr();
     ASSERT_NE(expr, nullptr) << "expr is null";
     ASSERT_EQ(expr->type, AstType::BinExpr);
     auto bin = reinterpret_cast<BinExpr *>(expr.get());
+
     EXPECT_EQ(bin->lhs->type, AstType::ValExpr);
-    EXPECT_EQ(bin->rhs->type, AstType::ValExpr);
-  } catch (const std::exception &e) {
-    ASSERT_EQ(1, 0) << "crashed";
-  }
-}
-TEST(parser, addition) {
-  FusionCtx ctx;
-  std::string code = "2 + 3 * 2\n";
-  auto file = FSFile("test", code);
-  auto lexer = Lexer(file, ctx);
-  lexer.lex();
-  auto tokens = lexer.tokens;
-  EXPECT_EQ(tokens.size(), 5);
-  Token::Type tks[]{Token::Lit, Token::Add, Token::Lit, Token::Mul, Token::Lit};
-  for (int i = 0; i < tokens.size(); i++) {
-    EXPECT_EQ(tokens[i].type, tks[i]) << "i: " << i;
-  }
-  /*
-          *
-        /   \
-       +     2
-      / \
-     2   3
-  */
-  auto p = Parser(tokens, ctx, file);
-  auto expr = p.parse_expr();
-  ASSERT_NE(expr, nullptr) << "expr is null";
-  ASSERT_EQ(expr->type, AstType::BinExpr);
-  auto bin = reinterpret_cast<BinExpr *>(expr.get());
 
-  EXPECT_EQ(bin->lhs->type, AstType::ValExpr);
-
-  ASSERT_EQ(bin->rhs->type, AstType::BinExpr);
-  auto r = reinterpret_cast<BinExpr *>(bin->rhs.get());
-  EXPECT_EQ(r->lhs->type, AstType::ValExpr);
-  EXPECT_EQ(r->rhs->type, AstType::ValExpr);
+    ASSERT_EQ(bin->rhs->type, AstType::BinExpr);
+    auto r = reinterpret_cast<BinExpr *>(bin->rhs.get());
+    EXPECT_EQ(r->lhs->type, AstType::ValExpr);
+    EXPECT_EQ(r->rhs->type, AstType::ValExpr);
 }
 TEST(parser, binexpr) {
-  FusionCtx ctx;
-  std::string code = "5 = 3 + 2";
-  auto file = FSFile("test", code);
-  auto lexer = Lexer(file, ctx);
-  lexer.lex();
-  auto tokens = lexer.tokens;
-  EXPECT_EQ(tokens.size(), 5);
-  Token::Type tks[]{Token::Lit, Token::Eq, Token::Lit, Token::Add, Token::Lit};
-  for (int i = 0; i < tokens.size(); i++) {
-    EXPECT_EQ(tokens[i].type, tks[i]) << "i: " << i;
-  }
-  /*
-          =
-        /   \
-       5     +
-           /   \
-          3     2
-  */
-  auto p = Parser(tokens, ctx, file);
-  auto expr = p.parse_expr();
-  ASSERT_NE(expr, nullptr) << "expr is null";
-  ASSERT_EQ(expr->type, AstType::BinExpr);
-  auto bin = reinterpret_cast<BinExpr *>(expr.get());
-  EXPECT_EQ(bin->lhs->type, AstType::ValExpr) << "left should be varexpr (a=)";
-  ASSERT_EQ(bin->rhs->type, AstType::BinExpr) << "right side should be 3+2";
-  /*
-  auto r = reinterpret_cast<BinExpr*>(bin->rhs.get());
-  EXPECT_EQ(r->op,Token::Add) << "Expected a +";
-  EXPECT_EQ(r->lhs->type,AstType::ValExpr) << "3 should be a val";
-  EXPECT_EQ(r->rhs->type,AstType::ValExpr) << "2 should be a type";
-  */
+    FusionCtx ctx;
+    std::string code = "5 = 3 + 2";
+    auto file = FSFile("test", code);
+    auto lexer = Lexer(file, ctx);
+    lexer.lex();
+    auto tokens = lexer.tokens;
+    EXPECT_EQ(tokens.size(), 5);
+    Token::Type tks[] {Token::Lit, Token::Eq, Token::Lit, Token::Add, Token::Lit};
+    for (int i = 0; i < tokens.size(); i++) {
+        EXPECT_EQ(tokens[i].type, tks[i]) << "i: " << i;
+    }
+    /*
+            =
+          /   \
+         5     +
+             /   \
+            3     2
+    */
+    auto p = Parser(tokens, ctx, file);
+    auto expr = p.parse_expr();
+    ASSERT_NE(expr, nullptr) << "expr is null";
+    ASSERT_EQ(expr->type, AstType::BinExpr);
+    auto bin = reinterpret_cast<BinExpr *>(expr.get());
+    EXPECT_EQ(bin->lhs->type, AstType::ValExpr) << "left should be varexpr (a=)";
+    ASSERT_EQ(bin->rhs->type, AstType::BinExpr) << "right side should be 3+2";
+    /*
+    auto r = reinterpret_cast<BinExpr*>(bin->rhs.get());
+    EXPECT_EQ(r->op,Token::Add) << "Expected a +";
+    EXPECT_EQ(r->lhs->type,AstType::ValExpr) << "3 should be a val";
+    EXPECT_EQ(r->rhs->type,AstType::ValExpr) << "2 should be a type";
+    */
 }
