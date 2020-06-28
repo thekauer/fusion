db86263e855c93951e22e585ed53eb823d87340b
thekauer <thekauer@gmail.com>
2020-06-28 12:39:08 +0200

    use a for loop

--- test/test_lex.cpp
+++ test/test_lex.cpp
@@ -7,133 +7,29 @@ TEST(Lex, Keywords) {
   // test _
   std::string code = "fn for i8 i16 i32 i64 string if import export extern mod "
                      "true false bool";
+  std::vector<Kw_e> tkns{ Kw_e::Fn,Kw_e::For,Kw_e::I8,Kw_e::I16,Kw_e::I32,Kw_e::I64,Kw_e::String,Kw_e::If,Kw_e::Import,Kw_e::Export,Kw_e::Extern,Kw_e::Module,Kw_e::True,Kw_e::False,Kw_e::Bool };
   auto file = FSFile("", code);
   Lexer l = Lexer(file);
-  auto t = l.next();
-  EXPECT_EQ(t.type, Token::Kw);
-  EXPECT_EQ(t.getKw(), Kw_e::Fn);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Kw);
-  EXPECT_EQ(t.getKw(), Kw_e::For);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Kw);
-  EXPECT_EQ(t.getKw(), Kw_e::I8);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Kw);
-  EXPECT_EQ(t.getKw(), Kw_e::I16);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Kw);
-  EXPECT_EQ(t.getKw(), Kw_e::I32);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Kw);
-  EXPECT_EQ(t.getKw(), Kw_e::I64);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Kw);
-  EXPECT_EQ(t.getKw(), Kw_e::String);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Kw);
-  EXPECT_EQ(t.getKw(), Kw_e::If);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Kw);
-  EXPECT_EQ(t.getKw(), Kw_e::Import);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Kw);
-  EXPECT_EQ(t.getKw(), Kw_e::Export);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Kw);
-  EXPECT_EQ(t.getKw(), Kw_e::Extern);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Kw);
-  EXPECT_EQ(t.getKw(), Kw_e::Module);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Kw);
-  EXPECT_EQ(t.getKw(), Kw_e::True);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Kw);
-  EXPECT_EQ(t.getKw(), Kw_e::False);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Kw);
-  EXPECT_EQ(t.getKw(), Kw_e::Bool);
+  for (auto expected : tkns) {
+      auto t = l.next();
+      EXPECT_EQ(t.type, Token::Kw);
+      EXPECT_EQ(t.getKw(), expected);
+  }
 }
 
 TEST(Lex, Operator) {
   std::string code = "+ - * / % ^ ! = & | ( ) # . .. ... : ; ? \\ [ ] { } _ ~ "
                      "== != >= <= += -= /= %= *= ~=";
-  // std::vector<Token::Type> correct {Token::Add,Token::Sub,Token::Mul};
+  std::vector<Token::Type> correct {Token::Add,Token::Sub,Token::Mul,Token::Div,Token::Mod,Token::Xor,Token::Not,Token::Eq,
+  Token::And,Token::Or,Token::Lp,Token::Rp,Token::Hashtag,Token::Dot,Token::DotDot,Token::DotDotDot,Token::DoubleDot,Token::SemiColon,
+  Token::Questionmark,Token::Backslash,Token::Lb,Token::Rb,Token::Lc,Token::Rc,Token::Underscore,Token::Neg,Token::EqEq,Token::NotEq,
+  Token::GtEq,Token::LtEq,Token::AddEq,Token::SubEq,Token::DivEq,Token::ModEq,Token::MulEq,Token::NegEq};
   auto file = FSFile("", code);
   Lexer l = Lexer(file);
-  auto t = l.next();
-  EXPECT_EQ(t.type, Token::Add);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Sub);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Mul);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Div);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Mod);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Xor);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Not);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Eq);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::And);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Or);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Lp);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Rp);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Hashtag);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Dot);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::DotDot);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::DotDotDot);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::DoubleDot);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::SemiColon);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Questionmark);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Backslash);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Lb);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Rb);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Lc);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Rc);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Underscore);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::Neg);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::EqEq);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::NotEq);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::GtEq);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::LtEq);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::AddEq);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::SubEq);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::DivEq);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::ModEq);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::MulEq);
-  t = l.next();
-  EXPECT_EQ(t.type, Token::NegEq);
+  for (auto expected : correct) {
+      auto t = l.next();
+      EXPECT_EQ(t.type, expected);
+  }
 }
 
 TEST(Lex, OneInOther) {
