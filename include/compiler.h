7ffb0ff83ae1d2c01ccacdde3e7e2eb80ee94d07
Restyled.io <commits@restyled.io>
2020-08-09 15:44:31 +0000

    Restyled by clang-format

--- include/compiler.h
+++ include/compiler.h
@@ -7,12 +7,12 @@
 
 class Compiler {
 public:
-    Compiler() = default;
-    void compile(int argc, char **argv);
-    void test();
+  Compiler() = default;
+  void compile(int argc, char **argv);
+  void test();
 
 private:
-    void mod_to_file(llvm::Module *m, const std::string &filename = "main.ll");
-    void create_fs_std_lib(FusionCtx &ctx);
-    void generate_obj(llvm::Module *m, const std::string &filename = "main.o");
+  void mod_to_file(llvm::Module *m, const std::string &filename = "main.ll");
+  void create_fs_std_lib(FusionCtx &ctx);
+  void generate_obj(llvm::Module *m, const std::string &filename = "main.o");
 };
