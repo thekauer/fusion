fdd1786e29f30a64cc3089df1fddca088c86c7bf
Restyled.io <commits@restyled.io>
2020-08-09 16:35:24 +0000

    Restyled by clang-format

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -1,293 +1,291 @@
+#include "error.h"
 #include "parser.h"
 #include "type.h"
 #include "llvm/IR/Verifier.h"
-#include "error.h"
 static bool type_check(llvm::Type *lhs, llvm::Type *rhs) {
-    if (rhs->isPointerTy()) {
-        return rhs->isPointerTy();
-    }
-    if (lhs->isIntegerTy()) {
-        return rhs->isIntegerTy();
-    }
-    if (lhs->isFloatingPointTy()) {
-        return rhs->isFloatingPointTy();
-    }
-    return false;
+  if (rhs->isPointerTy()) {
+    return rhs->isPointerTy();
+  }
+  if (lhs->isIntegerTy()) {
+    return rhs->isIntegerTy();
+  }
+  if (lhs->isFloatingPointTy()) {
+    return rhs->isFloatingPointTy();
+  }
+  return false;
 }
 static llvm::Value *alloc(FusionCtx &ctx, llvm::Type *ty, std::string name) {
-    llvm::AllocaInst *val = ctx.builder.CreateAlloca(ty, nullptr, name);
-    ctx.named_values[name] = val;
-    return val;
+  llvm::AllocaInst *val = ctx.builder.CreateAlloca(ty, nullptr, name);
+  ctx.named_values[name] = val;
+  return val;
 }
 
 llvm::Value *FnCall::codegen(FusionCtx &ctx) const {
-    auto *fn = ctx.mod->getFunction(name);
-    if (!fn) {
-        std::string err_msg = "unknown function called " + name;
-        Error::ImplementMe(err_msg);
-        return nullptr;
+  auto *fn = ctx.mod->getFunction(name);
+  if (!fn) {
+    std::string err_msg = "unknown function called " + name;
+    Error::ImplementMe(err_msg);
+    return nullptr;
+  }
+  std::vector<llvm::Value *> fn_args;
+  for (auto &&arg : args) {
+    if (arg) {
+      fn_args.push_back(arg->codegen(ctx));
     }
-    std::vector<llvm::Value *> fn_args;
-    for (auto &&arg : args) {
-        if (arg) {
-            fn_args.push_back(arg->codegen(ctx));
-        }
-    }
-    return ctx.builder.CreateCall(fn, fn_args, "call");
+  }
+  return ctx.builder.CreateCall(fn, fn_args, "call");
 }
 
 llvm::Value *FnProto::codegen(FusionCtx &ctx) const {
-    std::vector<llvm::Type *> fn_args;
-    for (auto &&arg : args) {
-        if (arg->type == AstType::VarDeclExpr) {
-            auto vd = reinterpret_cast<VarDeclExpr *>(arg.get());
-            fn_args.push_back(vd->ty.get_type_ptr()->codegen(ctx));
-        }
+  std::vector<llvm::Type *> fn_args;
+  for (auto &&arg : args) {
+    if (arg->type == AstType::VarDeclExpr) {
+      auto vd = reinterpret_cast<VarDeclExpr *>(arg.get());
+      fn_args.push_back(vd->ty.get_type_ptr()->codegen(ctx));
     }
+  }
 
+  llvm::Type *ret_t = ctx.getI32();
+  llvm::GlobalValue::LinkageTypes lt = llvm::Function::ExternalLinkage;
+  llvm::FunctionType *ft = llvm::FunctionType::get(ret_t, fn_args, false);
 
-    llvm::Type *ret_t = ctx.getI32();
-    llvm::GlobalValue::LinkageTypes lt = llvm::Function::ExternalLinkage;
-    llvm::FunctionType *ft = llvm::FunctionType::get(ret_t, fn_args, false);
+  llvm::Function *f = llvm::Function::Create(ft, lt, name, ctx.mod.get());
+  // set arg names
+  unsigned idx = 0;
+  for (auto &arg : f->args()) {
+    auto n = ((VarDeclExpr *)(args[idx++].get()))->name;
+    arg.setName(n);
+    ;
+  }
 
-    llvm::Function *f = llvm::Function::Create(ft, lt, name, ctx.mod.get());
-    // set arg names
-    unsigned idx = 0;
-    for (auto &arg : f->args()) {
-        auto n = ((VarDeclExpr *)(args[idx++].get()))->name;
-        arg.setName(n);
-        ;
-    }
-
-    return f;
+  return f;
 }
 
 llvm::Value *FnDecl::codegen(FusionCtx &ctx) const {
-    auto *p = (llvm::Function *)proto->codegen(ctx);
+  auto *p = (llvm::Function *)proto->codegen(ctx);
 
-    auto fname = proto->name;
-    auto *fn = (llvm::Function *)p;
-    if (mods & FnModifiers::Extern) {
-        ctx.mod->getOrInsertFunction(fname, p->getFunctionType());
-        return fn;
-    }
-    if (!fn) {
-        std::string err_msg = "unknown function called " + fname;
-        Error::ImplementMe(err_msg);
-        return nullptr;
-    }
-    llvm::BasicBlock *bb = llvm::BasicBlock::Create(ctx.ctx, "entry", fn);
-    ctx.builder.SetInsertPoint(bb);
-    ctx.named_values.clear();
-    for (auto &arg : fn->args()) {
-        llvm::AllocaInst *alloca =
-            ctx.builder.CreateAlloca(arg.getType(), nullptr, arg.getName());
-        ctx.builder.CreateStore(&arg, alloca);
-        ctx.named_values[arg.getName().data()] = alloca; // probably buggy
-    }
-
-    body->codegen(ctx);
-
-
-    llvm::verifyFunction(*fn);
+  auto fname = proto->name;
+  auto *fn = (llvm::Function *)p;
+  if (mods & FnModifiers::Extern) {
+    ctx.mod->getOrInsertFunction(fname, p->getFunctionType());
     return fn;
+  }
+  if (!fn) {
+    std::string err_msg = "unknown function called " + fname;
+    Error::ImplementMe(err_msg);
+    return nullptr;
+  }
+  llvm::BasicBlock *bb = llvm::BasicBlock::Create(ctx.ctx, "entry", fn);
+  ctx.builder.SetInsertPoint(bb);
+  ctx.named_values.clear();
+  for (auto &arg : fn->args()) {
+    llvm::AllocaInst *alloca =
+        ctx.builder.CreateAlloca(arg.getType(), nullptr, arg.getName());
+    ctx.builder.CreateStore(&arg, alloca);
+    ctx.named_values[arg.getName().data()] = alloca; // probably buggy
+  }
+
+  body->codegen(ctx);
+
+  llvm::verifyFunction(*fn);
+  return fn;
 }
 llvm::Value *ValExpr::codegen(FusionCtx &ctx) const {
-    auto const &type = val.ty.get_type();
-    if (type.get_typekind() != Type::Integral) {
-        return nullptr;
-    }
-    auto const &it = static_cast<const IntegralType &>(type);
-    switch (it.ty) {
-    case IntegralType::String: {
-        llvm::Type *i8ty = llvm::IntegerType::getInt8Ty(ctx.ctx);
-        llvm::ArrayType *sty = llvm::ArrayType::get(i8ty, val.as.string.size() + 1);
-        std::vector<llvm::Constant *> vals;
-        llvm::GlobalVariable *gstr = new llvm::GlobalVariable(
-            *ctx.mod, sty, true, llvm::GlobalValue::PrivateLinkage, 0, "str");
-        gstr->setAlignment(1);
-        llvm::Constant *cstr =
-            llvm::ConstantDataArray::getString(ctx.ctx, val.as.string.data(), true);
-        gstr->setInitializer(cstr);
-        return (llvm::Constant *)llvm::ConstantExpr::getBitCast(
-                   gstr, ctx.getI8()->getPointerTo());
-    }
-    case IntegralType::I32: {
-        return llvm::ConstantInt::get(ctx.getI32(),
-                                      llvm::APInt(32, val.as.i32, true));
-    }
-    case IntegralType::F32: {
-        return llvm::ConstantFP::get(ctx.ctx, llvm::APFloat(val.as.f32));
-    }
-    case IntegralType::Bool: {
-        if (val.as.b) {
-            return llvm::ConstantInt::getTrue(ctx.ctx);
-        }
-        else {
-            return llvm::ConstantInt::getFalse(ctx.ctx);
-        }
-
-    }
-    default:
-        Error::ImplementMe("Implement codegeneration for this type in ValExpr::codegen");
-        return nullptr;
-    }
-
+  auto const &type = val.ty.get_type();
+  if (type.get_typekind() != Type::Integral) {
     return nullptr;
+  }
+  auto const &it = static_cast<const IntegralType &>(type);
+  switch (it.ty) {
+  case IntegralType::String: {
+    llvm::Type *i8ty = llvm::IntegerType::getInt8Ty(ctx.ctx);
+    llvm::ArrayType *sty = llvm::ArrayType::get(i8ty, val.as.string.size() + 1);
+    std::vector<llvm::Constant *> vals;
+    llvm::GlobalVariable *gstr = new llvm::GlobalVariable(
+        *ctx.mod, sty, true, llvm::GlobalValue::PrivateLinkage, 0, "str");
+    gstr->setAlignment(1);
+    llvm::Constant *cstr =
+        llvm::ConstantDataArray::getString(ctx.ctx, val.as.string.data(), true);
+    gstr->setInitializer(cstr);
+    return (llvm::Constant *)llvm::ConstantExpr::getBitCast(
+        gstr, ctx.getI8()->getPointerTo());
+  }
+  case IntegralType::I32: {
+    return llvm::ConstantInt::get(ctx.getI32(),
+                                  llvm::APInt(32, val.as.i32, true));
+  }
+  case IntegralType::F32: {
+    return llvm::ConstantFP::get(ctx.ctx, llvm::APFloat(val.as.f32));
+  }
+  case IntegralType::Bool: {
+    if (val.as.b) {
+      return llvm::ConstantInt::getTrue(ctx.ctx);
+    } else {
+      return llvm::ConstantInt::getFalse(ctx.ctx);
+    }
+  }
+  default:
+    Error::ImplementMe(
+        "Implement codegeneration for this type in ValExpr::codegen");
+    return nullptr;
+  }
+
+  return nullptr;
 }
 
 llvm::Value *TypeExpr::codegen(FusionCtx &ctx) const {
-    return reinterpret_cast<llvm::Value *>(ty.get_type_ptr()->codegen(ctx));
+  return reinterpret_cast<llvm::Value *>(ty.get_type_ptr()->codegen(ctx));
 }
 
 llvm::Value *BinExpr::codegen(FusionCtx &ctx) const {
-    switch (op) {
-    case Token::Eq: {
-        auto *vlhs = lhs->codegen(ctx);
-        auto *vrhs = rhs->codegen(ctx);
-        if (!vrhs) {
-            Error::ImplementMe("No value");
-            return nullptr;
-        }
-        if (vlhs == (llvm::Value *)~0) { // left hand side is an infered type decl
-            auto *ty = vrhs->getType();
-            if (lhs->type != AstType::VarExpr) {
-                Error::ImplementMe("expected left-hand side to be varexpr");
-                return nullptr;
-            }
-            std::string name = reinterpret_cast<VarExpr *>(lhs.get())->name;
-            auto *var = alloc(ctx, ty, name);
-            return ctx.builder.CreateStore(vrhs, var);
-        }
+  switch (op) {
+  case Token::Eq: {
+    auto *vlhs = lhs->codegen(ctx);
+    auto *vrhs = rhs->codegen(ctx);
+    if (!vrhs) {
+      Error::ImplementMe("No value");
+      return nullptr;
+    }
+    if (vlhs == (llvm::Value *)~0) { // left hand side is an infered type decl
+      auto *ty = vrhs->getType();
+      if (lhs->type != AstType::VarExpr) {
+        Error::ImplementMe("expected left-hand side to be varexpr");
+        return nullptr;
+      }
+      std::string name = reinterpret_cast<VarExpr *>(lhs.get())->name;
+      auto *var = alloc(ctx, ty, name);
+      return ctx.builder.CreateStore(vrhs, var);
+    }
 
-        if (!type_check(vrhs->getType(), vlhs->getType())) {
-            Error::ImplementMe("types don't match");
-            return nullptr;
-        }
+    if (!type_check(vrhs->getType(), vlhs->getType())) {
+      Error::ImplementMe("types don't match");
+      return nullptr;
+    }
 
-        llvm::Value *var = ctx.named_values[vlhs->getName()];
-        return ctx.builder.CreateStore(vrhs, var);
-        return vrhs;
-    }
-    case Token::Add: {
-        // fix
-        auto ity = llvm::IntegerType::getInt32Ty(ctx.ctx);
-        auto callee = ctx.mod->getOrInsertFunction("addi32i32", ity, ity, ity);
-        return ctx.builder.CreateCall(callee,
-        {lhs->codegen(ctx), rhs->codegen(ctx)});
-        return nullptr;
-    }
-    case Token::Mul: {
-        auto ity = llvm::IntegerType::getInt32Ty(ctx.ctx);
-        auto callee = ctx.mod->getOrInsertFunction("muli32i32", ity, ity, ity);
-        return ctx.builder.CreateCall(callee,
-        {lhs->codegen(ctx), rhs->codegen(ctx)});
-        return nullptr;
-    }
-    default:
-        Error::ImplementMe("Codegen: Unimplemented operator!");
-        return nullptr;
-    }
+    llvm::Value *var = ctx.named_values[vlhs->getName()];
+    return ctx.builder.CreateStore(vrhs, var);
+    return vrhs;
+  }
+  case Token::Add: {
+    // fix
+    auto ity = llvm::IntegerType::getInt32Ty(ctx.ctx);
+    auto callee = ctx.mod->getOrInsertFunction("addi32i32", ity, ity, ity);
+    return ctx.builder.CreateCall(callee,
+                                  {lhs->codegen(ctx), rhs->codegen(ctx)});
     return nullptr;
+  }
+  case Token::Mul: {
+    auto ity = llvm::IntegerType::getInt32Ty(ctx.ctx);
+    auto callee = ctx.mod->getOrInsertFunction("muli32i32", ity, ity, ity);
+    return ctx.builder.CreateCall(callee,
+                                  {lhs->codegen(ctx), rhs->codegen(ctx)});
+    return nullptr;
+  }
+  default:
+    Error::ImplementMe("Codegen: Unimplemented operator!");
+    return nullptr;
+  }
+  return nullptr;
 }
 
 llvm::Value *VarExpr::codegen(FusionCtx &ctx) const {
-    // if the variable was already declared load it
-    // else if it doesn't exists then return fullptr
-    if (ctx.named_values.find(name) != ctx.named_values.end()) {
-        llvm::Value *v = ctx.named_values[name.data()];
-        if (!v) {
-            // errro unknown value
-            return nullptr;
-        }
-        return ctx.builder.CreateLoad(v, name.data());
-    } else {
-        return (llvm::Value *)~0;
+  // if the variable was already declared load it
+  // else if it doesn't exists then return fullptr
+  if (ctx.named_values.find(name) != ctx.named_values.end()) {
+    llvm::Value *v = ctx.named_values[name.data()];
+    if (!v) {
+      // errro unknown value
+      return nullptr;
     }
-    return 0;
+    return ctx.builder.CreateLoad(v, name.data());
+  } else {
+    return (llvm::Value *)~0;
+  }
+  return 0;
 }
 
 llvm::Value *VarDeclExpr::codegen(FusionCtx &ctx) const {
-    if (!ty.get_type_ptr()) {
-        Error::ImplementMe("could not infer type of expression");
-        return nullptr;
-    }
-    if (ctx.named_values.find(name) != ctx.named_values.end()) {
-        return ctx.named_values[name.data()];
-    }
-    llvm::AllocaInst *val =
-        ctx.builder.CreateAlloca(ty.get_type_ptr()->codegen(ctx), nullptr, name);
-    ctx.named_values[name] = val;
-    return val;
+  if (!ty.get_type_ptr()) {
+    Error::ImplementMe("could not infer type of expression");
+    return nullptr;
+  }
+  if (ctx.named_values.find(name) != ctx.named_values.end()) {
+    return ctx.named_values[name.data()];
+  }
+  llvm::AllocaInst *val =
+      ctx.builder.CreateAlloca(ty.get_type_ptr()->codegen(ctx), nullptr, name);
+  ctx.named_values[name] = val;
+  return val;
 }
 
 llvm::Value *RangeExpr::codegen(FusionCtx &ctx) const {
-    Error::ImplementMe("implement RangeExpr");
-    return nullptr; // implement me
+  Error::ImplementMe("implement RangeExpr");
+  return nullptr; // implement me
 }
 
-llvm::Value* Body::codegen(FusionCtx& ctx) const {
-    for (auto const& line : body) {
-        line->codegen(ctx);
-    }
-    return nullptr;
+llvm::Value *Body::codegen(FusionCtx &ctx) const {
+  for (auto const &line : body) {
+    line->codegen(ctx);
+  }
+  return nullptr;
 }
 
 llvm::Value *IfStmt::codegen(FusionCtx &ctx) const {
-    auto* condv = condition->codegen(ctx);
-    if (!condv) {
-        return nullptr;
-    }
-    condv =ctx.builder.CreateICmpEQ(condv, llvm::ConstantInt::getTrue(ctx.ctx), "ifcond");
-    llvm::Function* func = ctx.builder.GetInsertBlock()->getParent();
+  auto *condv = condition->codegen(ctx);
+  if (!condv) {
+    return nullptr;
+  }
+  condv = ctx.builder.CreateICmpEQ(condv, llvm::ConstantInt::getTrue(ctx.ctx),
+                                   "ifcond");
+  llvm::Function *func = ctx.builder.GetInsertBlock()->getParent();
 
-    llvm::BasicBlock* thenbb = llvm::BasicBlock::Create(ctx.ctx, "then");
-    llvm::BasicBlock* elsebb = llvm::BasicBlock::Create(ctx.ctx, "else");
-    llvm::BasicBlock* mergebb = llvm::BasicBlock::Create(ctx.ctx, "merge");
+  llvm::BasicBlock *thenbb = llvm::BasicBlock::Create(ctx.ctx, "then");
+  llvm::BasicBlock *elsebb = llvm::BasicBlock::Create(ctx.ctx, "else");
+  llvm::BasicBlock *mergebb = llvm::BasicBlock::Create(ctx.ctx, "merge");
 
-    ctx.builder.CreateCondBr(condv, thenbb, elsebb);
+  ctx.builder.CreateCondBr(condv, thenbb, elsebb);
 
-    ctx.builder.SetInsertPoint(thenbb);
-    auto* thenv = body->codegen(ctx);
-    if (!thenv)
-        return nullptr;
-    ctx.builder.CreateBr(mergebb);
-    thenbb = ctx.builder.GetInsertBlock();
+  ctx.builder.SetInsertPoint(thenbb);
+  auto *thenv = body->codegen(ctx);
+  if (!thenv)
+    return nullptr;
+  ctx.builder.CreateBr(mergebb);
+  thenbb = ctx.builder.GetInsertBlock();
 
-    func->getBasicBlockList().push_back(elsebb);
-    ctx.builder.SetInsertPoint(elsebb);
+  func->getBasicBlockList().push_back(elsebb);
+  ctx.builder.SetInsertPoint(elsebb);
 
-    auto* elsev = else_body->codegen(ctx);
-    if (!elsev)
-        return nullptr;
+  auto *elsev = else_body->codegen(ctx);
+  if (!elsev)
+    return nullptr;
 
-    ctx.builder.CreateBr(mergebb);
-    elsebb = ctx.builder.GetInsertBlock();
+  ctx.builder.CreateBr(mergebb);
+  elsebb = ctx.builder.GetInsertBlock();
 
-    func->getBasicBlockList().push_back(mergebb);
-    ctx.builder.SetInsertPoint(mergebb);
-    auto* pn = ctx.builder.CreatePHI(llvm::Type::getDoubleTy(ctx.ctx), 2, "iftmp");
-    pn->addIncoming(thenv, thenbb);
-    pn->addIncoming(elsev, elsebb);
+  func->getBasicBlockList().push_back(mergebb);
+  ctx.builder.SetInsertPoint(mergebb);
+  auto *pn =
+      ctx.builder.CreatePHI(llvm::Type::getDoubleTy(ctx.ctx), 2, "iftmp");
+  pn->addIncoming(thenv, thenbb);
+  pn->addIncoming(elsev, elsebb);
 
-    return pn;
+  return pn;
 }
 
 llvm::Value *ImportExpr::codegen(FusionCtx &ctx) const {
-    // compile module
-    Error::ImplementMe("ImportExpr::codegen");
-    return nullptr;
+  // compile module
+  Error::ImplementMe("ImportExpr::codegen");
+  return nullptr;
 }
 
-llvm::Value* ReturnStmt::codegen(FusionCtx& ctx) const
-{
-    auto *vexpr = expr->codegen(ctx);
-    if (vexpr) {
-        return ctx.builder.CreateRet(vexpr);
-    }
-    return nullptr;
+llvm::Value *ReturnStmt::codegen(FusionCtx &ctx) const {
+  auto *vexpr = expr->codegen(ctx);
+  if (vexpr) {
+    return ctx.builder.CreateRet(vexpr);
+  }
+  return nullptr;
 }
-llvm::Value* ClassStmt::codegen(FusionCtx& ctx) const {
-    body->codegen(ctx);
-    return reinterpret_cast<llvm::Value*>(ty.get_type_ptr()->codegen(ctx));
+llvm::Value *ClassStmt::codegen(FusionCtx &ctx) const {
+  body->codegen(ctx);
+  return reinterpret_cast<llvm::Value *>(ty.get_type_ptr()->codegen(ctx));
 }
--- src/type.cpp
+++ src/type.cpp
@@ -18,73 +18,39 @@ static const IntegralType bool_ = IntegralType("bool", IntegralType::Bool, 1);
 static const IntegralType f32 = IntegralType("f32", IntegralType::F32, 4);
 static const IntegralType f64 = IntegralType("f64", IntegralType::F64, 8);
 
-unsigned int Type::get_size() const {
-    return size;
-}
+unsigned int Type::get_size() const { return size; }
 
-std::string_view Type::get_name() const {
-    return name;
-}
+std::string_view Type::get_name() const { return name; }
 
-Type::TypeKind Type::get_typekind() const {
-    return tk;
-}
+Type::TypeKind Type::get_typekind() const { return tk; }
 
-const IntegralType &Type::get_i8() {
-    return i8;
-}
+const IntegralType &Type::get_i8() { return i8; }
 
-const IntegralType &Type::get_i16() {
-    return i16;
-}
+const IntegralType &Type::get_i16() { return i16; }
 
-const IntegralType &Type::get_i32() {
-    return i32;
-}
+const IntegralType &Type::get_i32() { return i32; }
 
-const IntegralType &Type::get_i64() {
-    return i64;
-}
+const IntegralType &Type::get_i64() { return i64; }
 
-const IntegralType &Type::get_isize() {
-    return isize;
-}
-const IntegralType &Type::get_u8() {
-    return u8;
-}
+const IntegralType &Type::get_isize() { return isize; }
+const IntegralType &Type::get_u8() { return u8; }
 
-const IntegralType &Type::get_u16() {
-    return u16;
-}
+const IntegralType &Type::get_u16() { return u16; }
 
-const IntegralType &Type::get_u32() {
-    return u32;
-}
+const IntegralType &Type::get_u32() { return u32; }
 
-const IntegralType &Type::get_u64() {
-    return u64;
-}
+const IntegralType &Type::get_u64() { return u64; }
 
-const IntegralType &Type::get_usize() {
-    return usize;
-}
+const IntegralType &Type::get_usize() { return usize; }
 
-const IntegralType &Type::get_char() {
-    return u8;
-}
+const IntegralType &Type::get_char() { return u8; }
 
-const IntegralType &Type::get_bool() {
-    return bool_;
-}
+const IntegralType &Type::get_bool() { return bool_; }
 
-const IntegralType &Type::get_f32() {
-    return f32;
-}
-const IntegralType &Type::get_f64() {
-    return f64;
-}
+const IntegralType &Type::get_f32() { return f32; }
+const IntegralType &Type::get_f64() { return f64; }
 const IntegralType &Type::get_string() {
-    Error::ImplementMe("implement integral string type");
+  Error::ImplementMe("implement integral string type");
 }
 
 IntegralType::IntegralType(const std::string_view name, Ty ty,
@@ -92,97 +58,87 @@ IntegralType::IntegralType(const std::string_view name, Ty ty,
     : ty(ty), Type(name, Integral, size) {}
 
 llvm::Type *IntegralType::codegen(FusionCtx &ctx) const {
-    llvm::Type *ret;
-    switch (ty) {
-    case Bool:
-    case Char:
-    case U8:
-    case I8:
-        ret = ctx.getI8();
-        break;
-    case U16:
-    case I16:
-        ret = ctx.getI16();
-        break;
-    case U32:
-    case I32:
-        ret = ctx.getI32();
-        break;
-    case U64:
-    case I64:
-        ret = ctx.getI64();
-        break;
-    case String:
-        Error::ImplementMe("implement string literal code generation");
-    default:
-        return nullptr;
-    }
-    return ret;
+  llvm::Type *ret;
+  switch (ty) {
+  case Bool:
+  case Char:
+  case U8:
+  case I8:
+    ret = ctx.getI8();
+    break;
+  case U16:
+  case I16:
+    ret = ctx.getI16();
+    break;
+  case U32:
+  case I32:
+    ret = ctx.getI32();
+    break;
+  case U64:
+  case I64:
+    ret = ctx.getI64();
+    break;
+  case String:
+    Error::ImplementMe("implement string literal code generation");
+  default:
+    return nullptr;
+  }
+  return ret;
 }
 
-QualType::QualType(const Type &type) : type(&type) {};
+QualType::QualType(const Type &type) : type(&type){};
 QualType::QualType(const IntegralType &type)
     : type(&static_cast<const Type &>(type)) {}
 
 QualType QualType::to_val() {
-    ref = 0;
-    return *this;
+  ref = 0;
+  return *this;
 }
 QualType QualType::to_ref() {
-    ref = 1;
-    return *this;
+  ref = 1;
+  return *this;
 }
 QualType QualType::to_mut() {
-    mut = 1;
-    return *this;
+  mut = 1;
+  return *this;
 }
 QualType QualType::to_const() {
-    mut = 0;
-    return *this;
+  mut = 0;
+  return *this;
 }
 QualType QualType::to_optional() {
-    opt = 1;
-    return *this;
+  opt = 1;
+  return *this;
 }
 QualType QualType::to_notoptional() {
-    opt = 0;
-    return *this;
+  opt = 0;
+  return *this;
 }
-bool QualType::is_ref() const {
-    return ref == 1;
-}
-bool QualType::is_mut() const {
-    return mut == 1;
-}
-bool QualType::is_opt() const {
-    return opt == 1;
-}
+bool QualType::is_ref() const { return ref == 1; }
+bool QualType::is_mut() const { return mut == 1; }
+bool QualType::is_opt() const { return opt == 1; }
 
-const Type &QualType::get_type() const {
-    return *type;
-}
-const Type *QualType::get_type_ptr() const {
-    return type;
-}
+const Type &QualType::get_type() const { return *type; }
+const Type *QualType::get_type_ptr() const { return type; }
 
 QualType &QualType::operator=(const QualType &other) {
-    type = other.type;
-    mods = other.mods;
-    return *this;
+  type = other.type;
+  mods = other.mods;
+  return *this;
 }
 
-
-unsigned int StructType::get_struct_size(std::vector<QualType>& fields) {
-    unsigned int sum = 0;
-    for (const auto& t : fields) {
-        sum += t.get_type_ptr()->get_size();
-    }
-    return sum;
+unsigned int StructType::get_struct_size(std::vector<QualType> &fields) {
+  unsigned int sum = 0;
+  for (const auto &t : fields) {
+    sum += t.get_type_ptr()->get_size();
+  }
+  return sum;
 }
-llvm::Type* StructType::codegen(FusionCtx& ctx) const {
-    std::vector<llvm::Type*> tys;
-    for (const auto& t : fields) {
-        tys.push_back(t.get_type_ptr()->codegen(ctx));
-    }
-    return llvm::StructType::create(ctx.ctx,llvm::ArrayRef(tys),llvm::StringRef(name.data()),false);
+llvm::Type *StructType::codegen(FusionCtx &ctx) const {
+  std::vector<llvm::Type *> tys;
+  for (const auto &t : fields) {
+    tys.push_back(t.get_type_ptr()->codegen(ctx));
+  }
+  return llvm::StructType::create(ctx.ctx, llvm::ArrayRef(tys),
+                                  llvm::StringRef(name.data()), false);
 }
