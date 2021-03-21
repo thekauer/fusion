d86d58e2a2d75798bc1c3bc148458934d8c76da5
thekauer <thekauer@gmail.com>
2021-03-21 22:45:32 +0100

    added test for VarDeclExpr

--- test/test_parser.cpp
+++ test/test_parser.cpp
@@ -211,3 +211,49 @@ TEST(parser, ResolveType) {
   ASSERT_EQ(fn_body->body[0]->type, AstType::VarDeclExpr);
 
 }
+
+TEST(parser, VarDeclExpr) {
+  FusionCtx ctx;
+  std::string code = "fn main()\n a : i32\n b : i32 = 1\n";
+  auto file = FSFile("test", code);
+  auto lexer = Lexer(file);
+  lexer.lex();
+  auto tokens = lexer.tokens;
+  Token::Type tks[]{Token::Kw,Token::Id,Token::Lp,Token::Rp,Token::Gi, // fn main()
+                    Token::Id,Token::DoubleDot, Token::Kw,Token::N, // a : i32
+                    Token::Id,Token::DoubleDot,Token::Kw,Token::Eq,Token::Lit,Token::Li // b : i32 = 1
+                    };
+  ASSERT_EQ(tokens.size(), sizeof(tks)/sizeof(tks[0]) );
+  for (int i = 0; i < tokens.size(); i++) {
+    ASSERT_EQ(tokens[i].type, tks[i]) << "i: " << i;
+  }
+  auto p = Parser(tokens, ctx, file);
+  auto tle = p.parse();
+  ASSERT_NE(tle, nullptr) << "top level expr is null";
+  ASSERT_EQ(tle->type, AstType::Body);
+  ASSERT_EQ(tle->body.size(), 1);
+  auto main = tle->body[0].get();
+  ASSERT_EQ(main->type, AstType::FnDecl);
+  auto body = main->cast<FnDecl>()->body.get();
+  ASSERT_EQ(body->body.size(), 2);
+  auto first = body->body[0].get();
+  ASSERT_EQ(first->type, AstType::VarDeclExpr);
+  auto second = body->body[1].get();
+  ASSERT_EQ(second->type, AstType::BinExpr);
+
+  auto first_vardecl = first->cast<VarDeclExpr>();
+  auto second_binexpr = second->cast<BinExpr>();
+
+  ASSERT_EQ(first_vardecl->name, "a");
+  ASSERT_EQ(first_vardecl->ty.get_type_ptr(), &Type::get_i32());
+
+  ASSERT_EQ(second_binexpr->lhs->type, AstType::VarDeclExpr);
+  ASSERT_EQ(second_binexpr->rhs->type, AstType::ValExpr);
+  auto second_lhs = second_binexpr->lhs->cast<VarDeclExpr>();
+  auto second_rhs = second_binexpr->rhs->cast<ValExpr>();
+
+  ASSERT_EQ(second_lhs->name, "b");
+  ASSERT_EQ(second_lhs->ty.get_type_ptr(), &Type::get_i32());
+  ASSERT_EQ(second_rhs->val.ty.get_type_ptr(), &Type::get_i32());
+  ASSERT_EQ(second_rhs->val.as.i32, 1);
+}
